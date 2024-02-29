83f066b03377b5a58bc6ec3b6a097cefacbaa4da
Hritik <pypi.pioneer@gmail.com>
2024-02-29 22:24:55 +0530

    redux slice refactored

--- src/features/user/userSlice.ts
+++ src/features/user/userSlice.ts
@@ -3,22 +3,40 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 // initial state type of the user data
 interface FormData {
+
+    // user personal data
     name: string | null;
     age: number | null;
     mobile: string | null;
     sex: "male" | "female" | null;
     idType: "aadhar" | "pan" | null;
     idNumber: string | null;
+
+    // user address data
+    address?: string;
+    state?: string;
+    city?: string;
+    country?: string;
+    pinCode?: number | null;
 }
 
 // initial state of the user
 const userInitialState: FormData = {
+    
+    // personal data of user
     name: null,
     age: null,
     mobile: null,
     sex: null,
     idType: null,
     idNumber: null,
+
+    // user address data
+    address: '',
+    state: '',
+    city: '',
+    country: '',
+    pinCode: null,
 }
 
 // now, create the action and reducer using slice
@@ -26,12 +44,25 @@ const userSlice = createSlice({
     name: 'user',
     initialState: userInitialState,
     reducers: {
-        setName: (state: FormData, action: PayloadAction<string>) => {},
-        setAge: (state, action: PayloadAction<number>) => {},
-        setMobile: (state, action: PayloadAction<string>) => {},
-        setSex: (state, action: PayloadAction<"male" | "female">) => {},
-        setIdType: (state, action: PayloadAction<"aadhar" | "pan">) => {},
-        setIdNumber: (state, action: PayloadAction<string>) => {},
+
+        // here we will set the personal data of the user
+        setUserPersonalData: (state, action: PayloadAction<FormData>) => {
+            state.name = action.payload.name;
+            state.age = action.payload.age;
+            state.mobile = action.payload.mobile;
+            state.sex = action.payload.sex;
+            state.idType = action.payload.idType;
+            state.idNumber = action.payload.idNumber;
+        },
+
+        // here we will set the address of the user
+        serUserAddress: (state, action: PayloadAction<FormData>) => {
+            state.address = action.payload.address;
+            state.state = action.payload.state;
+            state.city = action.payload.city;
+            state.country = action.payload.country;
+            state.pinCode = action.payload.pinCode;
+        }
     },
 });
 
